374fdc65d7b7ffbd5f1345296e63201a481b65cb
BURDET ETIENNE <etienne.burdet@gmail.com>
2019-12-17 14:28:28 +0100

    add basic routes

---
+++ pages/counts/_id/index.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <h1>Index de {{ id }}</h1>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
